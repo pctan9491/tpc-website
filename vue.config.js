07282ceeb629eecbcd46b592252d319575ff5d8b
pctan9491 <82195853+pctan9491@users.noreply.github.com>
2024-01-12 22:58:56 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/tpc-website" : "/", // 默认publicPath是'/'，如果发布到
+  publicPath: process.env.NODE_ENV === "production" ? "/tpc-website/" : "/", // 默认publicPath是'/'，如果发布到
 });
